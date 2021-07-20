63ff199ed8078475e85b28ef03a36c13c395f048
stella4545 <stellachepchumbah@gmail.com>
2021-07-20 19:31:34 +0300

    third commit

--- app/src/main/java/com/example/mycontactsapp/contactdetailactivity.kt
+++ app/src/main/java/com/example/mycontactsapp/contactdetailactivity.kt
@@ -2,19 +2,42 @@ package com.example.mycontactsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import com.squareup.picasso.Picasso
 
 class contactdetailactivity : AppCompatActivity() {
+    lateinit var tvName:TextView
+    lateinit var image:ImageView
+    lateinit var phone: TextView
+    lateinit var Email:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contactdetailactivity)
+        tvName.findViewById<TextView>(R.id.tvName)
+        phone.findViewById<TextView>(R.id.tvPhoneNum)
+        Email.findViewById<TextView>(R.id.tvEmail)
+        image.findViewById<ImageView>(R.id.imageView)
+
+
         var name=intent.getStringExtra("name")
-        Toast.makeText(baseContext,name,Toast.LENGTH_LONG).show()
+        var phone2=intent.getStringExtra("phoneNumber")
+        var Email2=intent.getStringExtra("Email")
+        var image2=intent.getStringExtra("image").toString()
 
-        var phone=intent.getStringExtra("phoneNumber")
-        Toast.makeText(baseContext,phone,Toast.LENGTH_LONG).show()
 
-        var Email=intent.getStringExtra("Email")
-        Toast.makeText(baseContext,Email,Toast.LENGTH_LONG).show()
+        tvName.text=name
+        phone.text=phone2
+        Email.text=Email2
+
+        Picasso.get()
+            .load(image2)
+            .into(image)
+
     }
+
+
 }
